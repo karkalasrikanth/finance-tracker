06893a73165a2f7cbac2b0bb457024c653d78d2c
karkalasrikanth <karkala.srikanth@gmail.com>
2026-04-19 01:20:48 -0400

    default working directory

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.17.0"
-    }
-  }
-}
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
